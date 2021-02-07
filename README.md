2aa148f5ef8c6d6af5ec3e0970c45d859cedc094
陈明 <chenming2@lppz.com>
2021-02-07 21:55:30 +0800

    获取推荐列表

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 
-
 api
 
 https://github.com/Binaryify/NeteaseCloudMusicApi
+
+PORT=4000 node app.js
+
+1, npx react-native start
+2, npx react-native run-ios
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,25 @@
 import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
-import {View, Text, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  FlatList,
+  Dimensions,
+} from 'react-native';
 import * as actionTypes from './store/actionCreators';
+
+import Swiper from 'react-native-swiper';
+
+const {width, height} = Dimensions.get('window');
 
 const Home = (props) => {
   const {bannerList, recommendList} = props;
-
   const {getBannerDataDispatch, getRecommendListDataDispatch} = props;
-
-  console.log(' bannerList ----> ', bannerList);
-  console.log(' recommendList ----> ', recommendList);
+  // console.log('->>>>>', bannerList);
+  // console.log('------>', recommendList);
 
   useEffect(() => {
     getBannerDataDispatch();
@@ -20,7 +30,33 @@ const Home = (props) => {
   return (
     <SafeAreaView>
       <View>
-        <Text> hello </Text>
+        <Text>Hello world</Text>
+        <View style={styles.image}>
+          <Swiper style={styles.image} autoplay>
+            {bannerList.map((item) => {
+              console.log('swiper -----------> ', item.imageUrl);
+              <View style={styles.image}>
+                <Image style={styles.image} source={{uri: item.imageUrl}} />
+              </View>;
+            })}
+          </Swiper>
+        </View>
+        <FlatList
+          data={recommendList}
+          horizontal={false}
+          numColumns={3}
+          keyExtractor={(item) => item.id + ''}
+          renderItem={(item) => {
+            return (
+              <View>
+                <Image
+                  style={styles.cellImage}
+                  source={{uri: item.item.picUrl}}
+                />
+              </View>
+            );
+          }}
+        />
       </View>
     </SafeAreaView>
   );
@@ -41,5 +77,20 @@ const mapDispatchToProps = (dispatch) => {
     },
   };
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: '#f00',
+    height: 100,
+  },
+  image: {
+    width,
+    height: 100,
+  },
+  cellImage: {
+    width: width / 3,
+    height: width / 3,
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
